4b0933546a53d112ec63e4f7373f15cacc0f34ad
Yaroslav <yar21sav@gmail.com>
2023-08-16 15:52:17 +0300

    add second realsense

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -21,8 +21,6 @@ docker run -ti --rm \
       -v $ROOT_DIR:/workspace \
       -v $ROOT_DIR/cache:/root/.cache \
       -v $ROOT_DIR/ros_ws/src/ds4drv/udev/50-ds4drv.rules:/etc/udev/rules.d/50-ds4drv.rules \
-      --device /dev/snd \
-      --device /dev/usb \
       --net=host \
       --privileged \
       --name saycan_exp saycan_exp-img \
